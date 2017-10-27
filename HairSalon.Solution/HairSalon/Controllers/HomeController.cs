b2acdff3a28189f6c8dfedeb24f1cde1fd912443
Adam Titus <adamtitus76@gmail.com>
2017-10-27 13:48:02 -0700

    Add stylist form and stylist saving capabilities.

--- HairSalon.Solution/HairSalon/Controllers/HomeController.cs
+++ HairSalon.Solution/HairSalon/Controllers/HomeController.cs
@@ -13,5 +13,17 @@ namespace Salon.Controllers
         List<Stylist> stylists = Stylist.GetAll();
         return View(stylists);
       }
+      [HttpGet("/stylist")]
+      public ActionResult AddStylist()
+      {
+        return View();
+      }
+      [HttpPost("/stylist/new")]
+      public ActionResult NewStylist()
+      {
+        Stylist newStylist = new Stylist(Request.Form["new-stylist"]);
+        newStylist.Save();
+        return Redirect("/");
+      }
     }
 }
